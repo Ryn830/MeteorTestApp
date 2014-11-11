a050441af6e0998d7649d06245b30fa874c48048
Ryn830 <Ryn830@gmail.com>
2014-11-10 20:41:20 -0800

    (feat) Adds ability to submit posts

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,14 @@
+Template.postSubmit.events({
+  'submit form': function(element) {
+    console.log(element);
+    element.preventDefault();
+
+    var post = {
+      url: $(element.target).find('[name=url]').val(),
+      title: $(element.target).find('[name=title]').val()
+    };
+
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
+});
