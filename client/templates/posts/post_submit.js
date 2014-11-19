3fdbbf9cba832de664650295b0a4d08fcddac6e2
Ryn830 <Ryn830@gmail.com>
2014-11-19 13:14:23 -0800

    (feat) Adds client form validation for empty post titles and urls

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,10 +7,26 @@ Template.postSubmit.events({
       title: $(element.target).find('[name=title]').val()
     };
 
+    var errors = validatePost(post);
+    if(errors.title || errors.url) return Session.set('postSubmitErrors', errors);
+
     Meteor.call('postInsert', post, function(error, result){
       if(error) return throwError(error.reason);
       if(result.postExists) throwError('This link has already been posted');
       Router.go('postsList', post);
     });
+  }
+});
+
+Template.postSubmit.created = function(){
+  Session.set('postSubmitErrors', {});
+};
+
+Template.postSubmit.helpers({
+  errorMessage: function(field){
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field){
+    return Boolean(Session.get('postSubmitErrors')[field]) ? 'has-error' : '';
   }
 });
