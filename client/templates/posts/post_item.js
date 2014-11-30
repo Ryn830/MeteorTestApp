7dff6b11bff007db8518dde7efa06a82773a5155
Ryn830 <ryn830@gmail.com>
2014-11-30 12:43:08 -0800

    (feat) Greys out upvote link when not logged in/already voted

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,16 +1,23 @@
 Template.postItem.helpers({
-  ownPost: function() {
+  ownPost: function(){
     return this.userId === Meteor.userId();
   },
-  domain: function() {
+  domain: function(){
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  },
+  upvotedClass: function(){
+    var userId = Meteor.userId();
+    if(userId && !_.include(this.upvoters, userId)){
+      return 'btn-primary upvotable';
+    }
+    return 'disabled';
   }
 });
 
 Template.postItem.events({
-  'click .upvote': function(event){
+  'click .upvotable': function(event){
     event.preventDefault();
     Meteor.call('upvote', this._id);
   }
