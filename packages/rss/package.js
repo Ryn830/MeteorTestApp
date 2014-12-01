b9ba721887653a7b6dcbd2d3510c6e5647b9d444
Ryn830 <Ryn830@gmail.com>
2014-12-01 13:52:32 -0800

    (feat) Creates npm RSS package wrapper and a basic feed

---
+++ packages/rss/package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'rss',
+  summary: 'RSS feed generator',
+  version: '0.1.0'
+});
+
+Npm.depends({rss: '1.0.1'});
+
+Package.onUse(function(api) {
+  api.versionsFrom('0.9.4');
+  api.addFiles('rss.js', 'server');
+  api.export('RSS');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('rss');
+  api.addFiles('rss-tests.js');
+});
