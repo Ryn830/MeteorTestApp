abba3ba32d0e63b792d79347779bd178a4be9a0c
Ryn830 <Ryn830@gmail.com>
2014-12-02 10:39:55 -0800

    (feat) Bitly package calls bitly.com api

--- packages/bitly/bitly.js
+++ packages/bitly/bitly.js
@@ -1,1 +1,25 @@
-// Write your package code here!
+Bitly = {};
+
+Bitly.shortenURL = function(url){
+  if(!Meteor.settings.bitly){
+    throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings');
+
+    var shortenResponse = Meteor.http.get(
+      'https://api-ssl.bitly.com/v3/shorten?',
+      {
+        timeout: 5000,
+        params: {
+          'format': 'json',
+          'access_token': Meteor.settings.bitly,
+          'longUrl': url
+        }
+      }
+    );
+
+    if(shortenResponse.statusCode !== 200){
+      throw new Meteor.Error(500, 'Bitly call failed with error: ' + shortenResponse.status_txt);
+    }
+
+    return shortenResponse.data.data.url;
+  }
+}
